6ca0d03a9e21fd8558de208ebe812031dea370a7
Dave Arnold <dave.arnold@clover.com>
2022-05-06 17:57:13 -0700

    still working on inventory

--- main/rentals/admin.py
+++ main/rentals/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from rentals.models import Equipment, EquipmentAccessory, EquipmentMedia
+
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+class EquipmentAccessoryAdmin(admin.ModelAdmin):
+    pass
+
+class EquipmentMediatAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(EquipmentAccessory, EquipmentAccessoryAdmin)
+admin.site.register(EquipmentMedia, EquipmentMediatAdmin)
--- main/rentals/models.py
+++ main/rentals/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Equipment(models.Model):
+    equipment_type = models.CharField(max_length=32)
+    equipment_name = models.CharField(max_length=32)
+    count = models.IntegerField()
+    description = models.TextField()
+    original_cost = models.FloatField()
+    rental_rate = models.FloatField()
+    image = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.equipment_name
+
+class EquipmentMedia(models.Model):
+    equipment = models.ManyToManyField(Equipment)
+    media = models.URLField()
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class EquipmentAccessory(Equipment):
+    pass

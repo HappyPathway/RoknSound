bf7617d82de338a8b18f6ea0447c5225ca979e50
Dave Arnold <dave.arnold@clover.com>
2022-05-07 20:31:26 -0700

    adding bootstrap css and js and disabling training and blog

--- main/main/context_processors.py
+++ main/main/context_processors.py
@@ -2,4 +2,8 @@ from django.conf import settings # import the settings file
 
 def template_settins(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'SITE_NAME': settings.SITE_NAME}
+    return {
+        'SITE_NAME': settings.SITE_NAME,
+        'BLOG_ENABLED': settings.BLOG_ENABLED,
+        'TRAINING_ENABLED': settings.TRAINING_ENABLED
+    }
--- main/main/settings.py
+++ main/main/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+BLOG_ENABLED=False
+TRAINING_ENABLED=False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
